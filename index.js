9545c660cab94565a710cbe92cc8e5e877c438d0
Mario Dickerson <139138591+daixintheazure@users.noreply.github.com>
2024-03-28 06:37:59 +0000

    more testing
    Tested the querySelectorAll

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ fetch("https://palworld.fandom.com/wiki/Paldeck")
   .then(html => {const root = parse(html); // Parse the HTML
                 // Now, you can manipulate the DOM using the 'root' which repesents the root of the parsed HTML document
                  //for example, to find all the links on the page, in a div with specified class you could do:
-                 const links = root.querySelectorAll('div.floatnone > a');
+                 const links = root.querySelectorAll('div.floatnone > a> img');
                  links.forEach(link => {
-                   console.log(link.getAttribute('href'));
+                   //console.log(link.getAttribute('href'));
+                   console.log(link.getAttribute('data-image-name'));
                    // Print the href attribute of each link
                  });
                 })
